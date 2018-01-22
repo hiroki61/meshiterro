7f18e53d1ed116602d54884f845bccea01b16429
北川弘貴 <hiroki.k@kitagawahiroshitakanoMacBook-puro.local>
2018-01-22 18:33:08 +0900

    Finish5-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   	resources :users
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
   	resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
+    resources :users, only: [:show]
    	end
 
    	root 'post_images#index'
